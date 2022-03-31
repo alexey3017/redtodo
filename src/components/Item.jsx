c0d97ac094bbdf00e3fc6a875e7425a6a9cccea9
aleksei <alexey3017@mail.ru>
2022-03-31 12:10:20 +0300

    add delete task

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {useState} from 'react';
+
 import {IconButton, Checkbox, ListItem, Typography} from '@mui/material';
 
 import EditIcon from '@mui/icons-material/Edit';
@@ -6,17 +8,38 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-export const Item = ({text, completed}) => {
+export const Item = ({text, completed, id, deleteTask}) => {
+
+    const [confirmDelete, setConfirmDelete] = useState(false);
+
+    const onDeleteClick = () => {
+        deleteTask(id);
+    };
+
+    const deleteQuestion = () => {
+        setConfirmDelete(true);
+    };
+
+    const onCancelDelete = () => {
+        setConfirmDelete(false);
+    };
     return (
         <ListItem>
             <div className="d-flex item">
                 <Checkbox checked={completed} icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon/>}/>
                 <Typography className="item-text">{text}</Typography>
+                {confirmDelete ? (
+                    <div>
+                        <span>Are you sure? </span>
+                        <button onClick={onDeleteClick}>Yes</button>
+                        <button onClick={onCancelDelete}>no</button>
+                    </div>
+                ) : null}
                 <div className="item-buttons d-flex">
                     <IconButton>
                         <EditIcon style={{fontSize: 20}}/>
                     </IconButton>
-                    <IconButton>
+                    <IconButton onClick={deleteQuestion}>
                         <DeleteOutlineIcon style={{fontSize: 20}}/>
                     </IconButton>
                 </div>
